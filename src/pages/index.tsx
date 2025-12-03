715ad1a23eb05fe083bc6c47a6ae1c038b857b20
Erwin Hoogerwoord <erwin@dialectic.design>
2025-12-03 08:24:43 +0100

    fixed main page intro link to correspond to github pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home(): JSX.Element {
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <main className="container container--narrow padding-vert--xl">
-        <p>Website with <a href="/docs/intro">documentation</a> for the Hyperobjects MacOS visual synthesis toolkit.</p>
+        <p>Website with <a href="/hyperobjects-macos-docs/docs/intro">documentation</a> for the Hyperobjects MacOS visual synthesis toolkit.</p>
         
         <p>See <a href="https://socratism.io">Socratism.io</a> for an impression of the capabilities.</p>
 
