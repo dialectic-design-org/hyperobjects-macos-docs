4a24945ee0659f3470aa031a568ed570920612a4
Erwin Hoogerwoord <erwin@dialectic.design>
2025-12-03 08:19:34 +0100

    updated url configs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://dialectic.design',
+  url: 'https://dialectic-design-org.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/hyperobjects-macos-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
